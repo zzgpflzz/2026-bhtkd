c6169fb68e5bb8dc5d3bbc62c65ee978fadefa7b
HyeriJang <hyeri.jang@linefriends.com>
2026-05-11 23:59:49 +0900

    chore: google search console verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
-import type { Metadata } from "next";
-import "./globals.css";
-
 export const metadata: Metadata = {
   title: "BAEKHO TAEKWONDO · 백호태권도",
-  description:
-    "한국체육대학교 부설 백호태권도 학부모용 심사 결과 조회 시스템",
+  description: "한국체육대학교 부설 백호태권도 학부모용 심사 결과 조회 시스템",
+  // 아래 내용을 추가하세요
+  verification: {
+    google: "nha2v8rsFE9_5o5vPV0_TTnx13_ZuAFXxFhhDhPcKQs",
+  },
 };
 
 export default function RootLayout({
