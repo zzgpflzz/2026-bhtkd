1b3d085f839e24cb2e9858aa826fcb5e1331c999
hyehye <z.hyeri.jang@gmail.com>
2026-05-30 13:31:14 +0900

    fix : admin 엑셀 추가 및 사진 일괄추가 기능 제거

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -260,6 +260,8 @@ export default function AdminPage() {
         </div>
       </header>
 
+      <div className="max-w-7xl mx-auto px-6 lg:px-8 py-8 sm:py-10">
+
         {dataLoading ? (
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 animate-fade-in">
             <div className="lg:col-span-1">
@@ -388,6 +390,7 @@ export default function AdminPage() {
           onDelete={() => handleDeleteExam(editingExam.id)}
         />
       )}
+      </div>
     </main>
   );
 }
